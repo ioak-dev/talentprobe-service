bda58ceea21610a7d4ef8acf10b77f79cdaf68a8
Anita Chaudhary <Anita.Chaudhary@westernacher.com>
2024-04-05 18:07:34 +0530

    Initial commit for authentication changes using public key

--- src/main/java/com/talentprobe/config/JwtSecurityConfig.java
+++ src/main/java/com/talentprobe/config/JwtSecurityConfig.java
@@ -1,21 +1,36 @@
 package com.talentprobe.config;
 
+import com.talentprobe.domain.authorization.JwtAuthenticationEntryPoint;
+import com.talentprobe.domain.authorization.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class JwtSecurityConfig {
 
+  @Autowired
+  private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+  @Autowired
+  private JwtRequestFilter jwtFilter;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf(AbstractHttpConfigurer::disable);
+    httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth
+            .anyRequest().authenticated())
+        .exceptionHandling(exp -> exp.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     return httpSecurity.build();
   }
 }
---
+++ src/main/java/com/talentprobe/domain/authorization/JwtRequestFilter.java
@@ -0,0 +1,99 @@
+package com.talentprobe.domain.authorization;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+  @Value("${jwt.publicKeyPath}")
+  private String publicKeyPath;
+
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse, FilterChain filterChain)
+      throws IOException {
+
+    try {
+      String token = decodeToken(httpServletRequest);
+      if (null != token) {
+        PublicKey publicKey = getPublicKey(publicKeyPath);
+
+        Claims claims = Jwts.parser().setSigningKey(publicKey).build().parseSignedClaims(token)
+            .getPayload();
+        String userId = claims.getSubject();
+        if (null != userId) {
+          UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+              userId, null, null);
+          SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+      }
+      filterChain.doFilter(httpServletRequest, httpServletResponse);
+    } catch (ExpiredJwtException expiredJwtException) {
+      httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+      httpServletResponse.getWriter().write("Token Expired");
+    } catch (MalformedJwtException exception) {
+      httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+      httpServletResponse.getWriter().write("Token Invalid");
+    } catch (Exception exception) {
+      httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+      httpServletResponse.getWriter().write("Unauthorized");
+    }
+  }
+
+  private PublicKey getPublicKey(String publicKeyPath) {
+    PublicKey publicKey = null;
+    Resource resource = new ClassPathResource(publicKeyPath);
+    try (BufferedReader reader = new BufferedReader(new FileReader(resource.getFile()))) {
+      StringBuilder pemContent = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+        if (line.startsWith("-----BEGIN PUBLIC KEY-----") || line.startsWith(
+            "-----BEGIN RSA PUBLIC KEY-----")) {
+          continue;
+        }
+        if (line.startsWith("-----END PUBLIC KEY-----") || line.startsWith(
+            "-----END RSA PUBLIC KEY-----")) {
+          continue;
+        }
+        pemContent.append(line);
+      }
+      byte[] decodedKey = Base64.getDecoder().decode(pemContent.toString());
+      X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
+      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+      publicKey = keyFactory.generatePublic(keySpec);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return publicKey;
+  }
+
+  private String decodeToken(HttpServletRequest httpServletRequest) {
+    String bearerToken = httpServletRequest.getHeader("Authorization");
+    if (null != bearerToken && bearerToken.startsWith("Bearer ")) {
+      return bearerToken.substring(7);
+    }
+    return null;
+  }
+}
