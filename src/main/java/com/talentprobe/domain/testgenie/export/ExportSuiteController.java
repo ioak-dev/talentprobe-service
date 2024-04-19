c4102178a809813896c4277fc73ec7865b85097b
shashanka.hegade <shashanka.hegade@westernacher.com>
2024-04-19 19:11:28 +0530

    added export endpoint for the test case

--- src/main/java/com/talentprobe/domain/testgenie/export/ExportSuiteController.java
+++ src/main/java/com/talentprobe/domain/testgenie/export/ExportSuiteController.java
@@ -1,18 +1,11 @@
 package com.talentprobe.domain.testgenie.export;
 
-import com.talentprobe.domain.testgenie.suite.Suite;
 import com.talentprobe.domain.testgenie.suite.SuiteService;
-import com.talentprobe.domain.testgenie.usecase.UseCase;
-import com.talentprobe.domain.testgenie.usecase.UseCaseService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +17,8 @@ public class ExportSuiteController {
   private SuiteService suiteService;
 
   @GetMapping("/{id}")
-  public void exportSuite(@PathVariable String suiteId){
-    suiteService.exportSuite(suiteId);
+  public ResponseEntity<ByteArrayResource> exportSuite(@PathVariable String id){
+   return suiteService.exportSuite(id);
   }
 
 }
--- src/main/java/com/talentprobe/domain/testgenie/testcase/TestCaseRepository.java
+++ src/main/java/com/talentprobe/domain/testgenie/testcase/TestCaseRepository.java
@@ -1,11 +1,11 @@
 package com.talentprobe.domain.testgenie.testcase;
 
-import com.talentprobe.domain.testgenie.usecase.UseCase;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TestCaseRepository extends MongoRepository<TestCase,String> {
   Optional<TestCase> findBySuiteIdAndUseCaseId(String suiteId, String usecaseId);
+
+  Optional<TestCase> findBySuiteId(String suiteId);
 
 }
