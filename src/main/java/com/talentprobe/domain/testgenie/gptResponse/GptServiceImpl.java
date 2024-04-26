e1a108d706507a5d9e5b7c09ffbba82d71dd5c8c
shashanka.hegade <shashanka.hegade@westernacher.com>
2024-04-26 17:24:49 +0530

    Fixed the test genie prompt response and jackson parse error

--- src/main/java/com/talentprobe/domain/testgenie/gptResponse/GptServiceImpl.java
+++ src/main/java/com/talentprobe/domain/testgenie/gptResponse/GptServiceImpl.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,16 +14,18 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @Service
-public class GptServiceImpl implements GptService{
+public class GptServiceImpl implements GptService {
 
   @Autowired
   private ResourceLoader resourceLoader;
@@ -45,18 +48,18 @@ public class GptServiceImpl implements GptService{
     List<GptResponse> gptResponseList = new ArrayList<>();
     try {
       HttpEntity<String> entity = createHttpEntity(usecase);
-      //ResponseEntity<Object> responseEntity = restTemplate.postForEntity(url, entity, Object.class);
-      Resource resource = resourceLoader.getResource("classpath:Gpt_Mock_Response_TestGenie.json");
+      ResponseEntity<Object> responseEntity = restTemplate.postForEntity(url, entity, Object.class);
+     /* Resource resource = resourceLoader.getResource("classpath:Gpt_Mock_Response_TestGenie.json");
       ResponseEntity<Object> responseEntity = ResponseEntity
           .ok()
           .header("header", "value")
           .body(StreamUtils.copyToString(resource.getInputStream(),
-              StandardCharsets.UTF_8));
+              StandardCharsets.UTF_8));*/
       gptResponseList = mapToGptResponse(responseEntity.getBody());
     } catch (RestClientException exception) {
       exception.printStackTrace();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,e.getMessage());
     }
 
     return gptResponseList;
@@ -87,10 +90,15 @@ public class GptServiceImpl implements GptService{
     ObjectMapper objectMapper = new ObjectMapper();
     List<GptResponse> list = new ArrayList<>();
     try {
-      JsonNode rootNode = objectMapper.readTree(body.toString());
-      JsonNode choicesNode = rootNode.get("choices");
-      if (choicesNode != null) {
-        if (choicesNode.isArray()) {
+      JsonNode rootNode = null;
+      if (body instanceof Map) {
+        rootNode = objectMapper.valueToTree(body);
+      } else if (body instanceof String) {
+        rootNode = objectMapper.readTree((String) body);
+      }
+      if (rootNode != null) {
+        JsonNode choicesNode = rootNode.get("choices");
+        if (choicesNode != null && choicesNode.isArray()) {
           for (JsonNode choice : choicesNode) {
             JsonNode messageNode = choice.get("message");
             if (messageNode != null) {
@@ -98,20 +106,22 @@ public class GptServiceImpl implements GptService{
               JsonNode contentNode = objectMapper.readTree(contentString);
               JsonNode testCaseNode = contentNode.get("testCases");
 
-              if(testCaseNode.isArray()){
-                for(JsonNode node :testCaseNode) {
+              if (testCaseNode.isArray()) {
+                for (JsonNode node : testCaseNode) {
                   GptResponse gptResponse = objectMapper.treeToValue(node, GptResponse.class);
                   list.add(gptResponse);
                 }
               }
             }
           }
+        } else {
+          log.error("'choices' node is missing in the response");
         }
-      } else {
-        log.error("'choices' node is missing in the response");
       }
     } catch (IOException e) {
       e.printStackTrace();
+    } catch (Exception e) {
+      log.error(e.getMessage());
     }
     return list;
   }
