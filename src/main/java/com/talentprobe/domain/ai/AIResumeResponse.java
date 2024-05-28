a6880dd52bbf517202d9a357b40d408a4fe78d72
Anita Chaudhary <Anita.Chaudhary@westernacher.com>
2024-05-28 17:43:02 +0530

    Removed Jackson mapping changes for resume scan and updated the prompt for the field keyQuestionsToBeAsked

--- src/main/java/com/talentprobe/domain/ai/AIResumeResponse.java
+++ src/main/java/com/talentprobe/domain/ai/AIResumeResponse.java
@@ -1,10 +1,5 @@
 package com.talentprobe.domain.ai;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
-import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,53 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class AIResumeResponse {
 
-  @JsonProperty("name")
-  private String name;
-
-  @JsonProperty("overview")
-  private String overview;
-
-  @JsonProperty("technicalSkills")
-  private Map<String, Object> technicalSkills;
-
-  @JsonProperty("domainSkills")
-  private Map<String, Object> domainSkills;
-
-  @JsonProperty("totalExperience")
-  private String totalExperience;
-
-  @JsonProperty("industryNormalizedExperience")
-  private String industryNormalizedExperience;
-
-  @JsonProperty("currentDesignation")
-  private String currentDesignation;
-
-  @JsonProperty("standardizedDesignation")
-  private String standardizedDesignation;
-
-  @JsonProperty("recentExperience")
-  private String recentExperience;
-
-  @JsonProperty("longestExperience")
-  private String longestExperience;
-
-  @JsonProperty("averageExperiencePerCompany")
-  private String avgExperiencePerCompany;
-
-  @JsonProperty("allProjects")
-  private List<String> allProjects;
-
-  @JsonProperty("keyProject")
-  private String keyProject;
-
-  @JsonProperty("keyQuestionsToBeAsked")
-  private Map<String, Object> questionsToBeAsked;
-
-  @JsonProperty("education")
-  private String education;
-
-  private Object output;
+  private Object data;
 }
