3bb5eb8b7d797c382fa55181c554173179c42e81
shashanka.hegade <shashanka.hegade@westernacher.com>
2024-06-24 18:06:20 +0530

    changed the model from gpt4 to gpt-4o

--- src/main/java/com/talentprobe/domain/util/GptModelSelector.java
+++ src/main/java/com/talentprobe/domain/util/GptModelSelector.java
@@ -1,19 +1,23 @@
 package com.talentprobe.domain.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class GptModelSelector {
 
   @Value("${enable.gpt4.model}")
   private boolean enableGpt4Model;
 
-  private static final String GPT4="gpt-4";
+  private static final String GPT4="gpt-4o";
 
-  private static final String GPT3="gpt-3.5-turbo-0125";
+  private static final String GPT3="gpt-3.5-turbo";
 
   public String getModelName() {
-    return enableGpt4Model ? GPT4 : GPT3;
+    String modelSelected= enableGpt4Model ? GPT4 : GPT3;
+    log.info("Model selected : "+modelSelected);
+    return modelSelected;
   }
 }
