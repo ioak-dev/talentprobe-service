640a4318c5e5a09e5d94f49c5e6794a9e1910c15
Arunkumar Selvaraj <arun@ioak.io>
2024-07-05 22:00:46 +0530

    Implementation of assessment related endpoints

---
+++ src/lib/gptutils.ts
@@ -0,0 +1,4 @@
+export const predict = async (count: number, text: string) => {
+  console.log("GPT prediction start");
+  console.log("GPT prediction end");
+};
--- src/route.ts
+++ src/route.ts
@@ -12,5 +12,7 @@ require("./modules/user/route")(router);
 require("./modules/user/invite/route")(router);
 require("./modules/company/route")(router);
 require("./modules/budget/route")(router);
+require("./modules/assessment/route")(router);
+require("./modules/assessment/question/route")(router);
 
 module.exports = router;
